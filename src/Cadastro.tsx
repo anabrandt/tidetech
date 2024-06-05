28c1293a30adeddbe25b07d36e451fc9848f60b9
Ana Carolina <anactavaresbrand@gmail.com>
2024-06-05 19:41:32 -0300

    fix: arrumando os formulários de cadastro e login

--- src/Cadastro.tsx
+++ src/Cadastro.tsx
@@ -1,75 +1,65 @@
-import { VStack, Image, Text, Box, Link } from 'native-base'
-import { TouchableOpacity } from 'react-native';
-import Logo from './assets/Logo.png'
+import { VStack, Image, Text, Box, Link, Checkbox, ScrollView } from 'native-base';
+import Logo from './assets/Logo.png';
 import { Botao } from './componentes/Botao';
 import { EntradaTexto } from './componentes/EntradaTexto';
 import { Titulo } from './componentes/Titulo';
 import { useState } from 'react';
+import { secoes } from './utils/CadastroEntradaTexto'
 
 export default function Login() {
   const [numSecao, setNumSecao] = useState(0);
-  const secoes = [
-    {
-      id: 1,
-      titulo: 'Insira alguns dados básicos',
-      entradaTexto: [
-        {
-          id: 1,
-          label: 'Nome',
-          placeholder: 'Digite seu nome completo'
-        },
-        {
-          id: 2,
-          label: 'Email',
-          placeholder: 'Digite seu melhor email'
-        }
-      ]
-    },
-    {
-      id: 2,
-      titulo: 'Agora, mais alguns dados sobre você:',
-      entradaTexto: [
-        {
-          id: 1,
-          label: 'CEP',
-          placeholder: 'Digite seu CEP'
-        }
-      ]
-    }
-  ]
 
-  function avancarSecao(){
-    if(numSecao < secoes.length - 1){
-      setNumSecao(numSecao+1)
+
+  function avancarSecao() {
+    if (numSecao < secoes.length - 1) {
+      setNumSecao(numSecao + 1);
     }
   }
-  
-  function voltarSecao(){
-    if(numSecao > 0){
-      setNumSecao(numSecao - 1)
+
+  function voltarSecao() {
+    if (numSecao > 0) {
+      setNumSecao(numSecao - 1);
     }
   }
 
   return (
-    <VStack flex={1} alignItems="center" p={5} justifyContent="center">
-      <Image source={Logo} alt="Logo TideTech" />
+    <ScrollView flex={1} p={5}>
+      <Image source={Logo} alt="Logo TideTech" alignSelf="center" />
 
       <Titulo>
         {secoes[numSecao].titulo}
       </Titulo>
       <Box>
-        {
-          secoes[numSecao].entradaTexto.map(entrada => {
-            return <EntradaTexto label={entrada.
-              label} placeholder={entrada.placeholder} key={entrada.id} />
-          })
-        }
+        {secoes[numSecao].entradaTexto &&
+          secoes[numSecao].entradaTexto.map(entrada => (
+            <EntradaTexto
+              label={entrada.label}
+              placeholder={entrada.placeholder}
+              key={entrada.id}
+            />
+          ))}
       </Box>
 
-      {numSecao > 0 && <Botao onPress={() => voltarSecao } bgColor="gray.400">Voltar</Botao>}
-      <Botao onPress={() => avancarSecao()} mt={4} mb={20}>Avançar</Botao>
+      <Box>
+        <Text color="blue.800" fontWeight="bold" fontSize="md" mt={2}>
+          Selecione a atividade:
+        </Text>
+        {secoes[numSecao].checkbox &&
+          secoes[numSecao].checkbox.map(checkbox => (
+            <Checkbox key={checkbox.id} value={checkbox.value}>
+              {checkbox.value}
+            </Checkbox>
+          ))}
+      </Box>
 
-    </VStack>
+      {numSecao > 0 && (
+        <Botao onPress={() => voltarSecao()} bgColor="gray.400">
+          Voltar
+        </Botao>
+      )}
+      <Botao onPress={() => avancarSecao()} mt={4} mb={20}>
+        Avançar
+      </Botao>
+    </ScrollView>
   );
 }
-
